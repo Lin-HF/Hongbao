c32d0bd27263455cd601ab7e8977ed84b2237950
David Lin <haiflin@iu.edu>
2019-07-16 23:03:16 -0400

    application properties

--- src/main/java/com/hflcode/luckymoney/HelloController.java
+++ src/main/java/com/hflcode/luckymoney/HelloController.java
@@ -1,16 +1,23 @@
 package com.hflcode.luckymoney;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 public class HelloController {
+
+    @Autowired
+    private LimitConfig limitConfig;
 
     @GetMapping("/hello")
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return "这是一个测试";
+        return "说明" + limitConfig.getDescription();
     }
 }
