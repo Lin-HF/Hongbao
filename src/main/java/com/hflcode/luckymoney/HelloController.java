ccfa993e49612dbb0c02f71f0b49a18a99b80e94
David Lin <haiflin@iu.edu>
2019-07-17 00:45:30 -0400

    database

--- src/main/java/com/hflcode/luckymoney/HelloController.java
+++ src/main/java/com/hflcode/luckymoney/HelloController.java
@@ -2,22 +2,32 @@ package com.hflcode.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
+
+@RequestMapping("/hello")
 @RestController
 public class HelloController {
 
     @Autowired
     private LimitConfig limitConfig;
 
-    @GetMapping("/hello")
-//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String say() {
-        return "说明" + limitConfig.getDescription();
+    @GetMapping("/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") int id) {
+//        return "说明" + limitConfig.getDescription();
+        return "id: " + id;
     }
+
+    @RequestMapping("/config")
+    public LimitConfig config () {
+        return limitConfig;
+    }
+
+//    @GetMapping("/hello2")
+//    public String say2() {
+//        return "index";
+//    }
 }
---
+++ src/main/java/com/hflcode/luckymoney/LuckymoneyRepository.java
@@ -0,0 +1,6 @@
+package com.hflcode.luckymoney;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LuckymoneyRepository extends JpaRepository<Luckymoney, Integer> {
+}
